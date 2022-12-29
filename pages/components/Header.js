ed300684a76eb06157bf34cc4f329a531c11da20
kuaysh <mustakhkuanysh@gmail.com>
2022-12-29 18:10:27 +0600

    deploy2

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -38,18 +38,7 @@ const Header = () => {
     <header className="bg-white dark:bg-black border-b border-[#ededed] dark:border-[#333]">
       <div className="container flex flex-col flex-wrap items-center p-5 mx-auto md:flex-row">
         <a className="flex items-center mb-4 font-medium text-gray-900 title-font md:mb-0">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            className="w-10 h-10 p-2 text-white bg-indigo-500 rounded-full"
-            viewBox="0 0 24 24"
-          >
-            <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-          </svg>
+         
           <Link href="/dashboard">
             <span className="ml-3 text-xl text-black cursor-pointer dark:text-white">
               Ario Admin
